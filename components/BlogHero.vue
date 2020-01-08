4e4e971ebc682e19d16c98a737ed541317668db0
dzigg <dfoxyz@live.com>
2020-01-08 14:44:58 +1000

    added heroku deploy

--- components/BlogHero.vue
+++ components/BlogHero.vue
@@ -4,7 +4,7 @@
       <div
         class="section-container z-10 relative flex flex-col sm:pt-8 lg:mt-12 lg:pt-12"
       >
-        <div class="w-full py-10 lg:pb-32">
+        <div class="w-full py-10 text-center lg:pb-32 xl:mx-auto xl:max-w-4xl">
           <span
             v-if="beforeText"
             class="inline-block px-3 py-2 bg-primary text-sm text-white font-bold tracking-wider xl:text-2xl"
@@ -14,13 +14,13 @@
 
           <h1
             v-html="title"
-            class="mt-6 text-gray-800 text-3xl leading-none tracking-wide font-bold xs:max-w-xs sm:max-w-md md:mt-12 md:text-5xl lg:max-w-3xl lg:text-6xl xl:max-w-6xl xl:text-7xl xxl:max-w-7xl xxl:text-8xl xxl:mt-12"
+            class="mt-6 text-gray-800 text-3xl leading-none tracking-wide font-bold "
           ></h1>
 
           <p
             v-if="content"
             v-html="content"
-            class="font-medium text-gray-700 text-base mt-10 inline-block rounded-lg md:text-lg xxl:text-4xl xxl:leading-tight xxl:font-bold"
+            class="font-medium text-gray-700 text-base mt-10 inline-block rounded-lg"
           ></p>
 
           <img
