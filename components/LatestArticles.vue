80fb311200479d429cf4cd8d0a0d93edcee6d8d6
dzigg <dfoxyz@live.com>
2020-01-08 11:25:34 +1000

    added better post listing styles, WIP

--- components/LatestArticles.vue
+++ components/LatestArticles.vue
@@ -1,49 +1,51 @@
 <template>
   <section :class="isDark ? 'bg-gray-900 py-12' : 'py-12'">
     <div class="section-container">
-      <img
-        class="mx-auto mb-6 mt-12 xl:w-20 xxl:w-32 xxl:mt-32"
-        src="https://cdn2.hubspot.net/hubfs/416563/AT/squiggle.svg"
-        alt="squiggle"
-      />
+      <div class=" xl:mx-auto xl:max-w-4xl">
+        <img
+          class="mb-6 mt-12 xl:w-20 xxl:w-32 xxl:mt-32"
+          src="https://cdn2.hubspot.net/hubfs/416563/AT/squiggle.svg"
+          alt="squiggle"
+        />
 
-      <h3
-        class="text-3xl font-bold text-center"
-        :class="isDark ? 'text-white' : 'text-gray-800'"
-      >
-        Latest articles
-      </h3>
-
-      <div
-        class="flex flex-col items-center mb-5 mt-12 md:flex-row md:justify-center md:items-start"
-      >
-        <div
-          v-for="article in postsContent.results"
-          :key="article.id"
-          class="flex flex-col justify-center items-start pt-48 bg-contain bg-no-repeat sm:max-w-sm md:mx-3 md:pt-56"
-          :style="
-            'background-image: url(' + article.data.header_image.url + ')'
-          "
+        <h3
+          class="text-3xl font-bold"
+          :class="isDark ? 'text-white' : 'text-gray-800'"
         >
-          <div class="bg-white p-4 md:w-full">
-            <nuxt-link :to="'/post/' + article.uid">
-              <h4
-                class="text-xl text-gray-800 font-bold xs:text-2xl md:text-xl"
-              >
-                {{ article.data.title[0].text }}
-              </h4>
-            </nuxt-link>
+          Latest articles
+        </h3>
 
-            <p
-              class="text-base text-gray-700 leading-relaxed tracking-wide font-medium my-4"
-            >
-              {{ article.data.foreword[0].text }}
-            </p>
-
-            <div class="relative">
-              <nuxt-link class="cta" :to="'/post/' + article.uid">
-                Read more
+        <div class="flex flex-col items-center mb-5 mt-12 md:items-start">
+          <div
+            v-for="article in postsContent.results"
+            :key="article.id"
+            class="flex flex-col justify-center items-stretch bg-contain bg-no-repeat w-full mb-12 md:flex-row"
+          >
+            <img
+              class="w-full object-cover md:w-1/2"
+              :src="article.data.header_image.url"
+              :alt="article.data.header_image.alt"
+            />
+            <div class="bg-white p-4 w-full md:w-1/2">
+              <nuxt-link :to="'/post/' + article.uid">
+                <h4
+                  class="text-xl text-gray-800 font-bold xs:text-2xl md:text-xl"
+                >
+                  {{ article.data.title[0].text }}
+                </h4>
               </nuxt-link>
+
+              <p
+                class="text-base text-gray-700 leading-relaxed tracking-wide font-medium my-4"
+              >
+                {{ article.data.foreword[0].text }}
+              </p>
+
+              <div class="relative">
+                <nuxt-link class="cta" :to="'/post/' + article.uid">
+                  Read more
+                </nuxt-link>
+              </div>
             </div>
           </div>
         </div>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <Navigation :navigationContent="navigationContent" />
-    <Hero
+    <BlogHero
       before-text="Blog"
-      :title='$prismic.richTextAsPlain(blogContent.headline)'
-      :content="$prismic.richTextAsPlain(blogContent.description)"  
+      :title="$prismic.richTextAsPlain(blogContent.headline)"
+      :content="$prismic.richTextAsPlain(blogContent.description)"
     />
 
     <LatestArticles :postsContent="postsContent" />
@@ -14,48 +14,49 @@
 </template>
 
 <script>
-import Prismic from "prismic-javascript"
-import PrismicConfig from "~/prismic.config.js"
-import Navigation from '~/components/Navigation'
-import Footer from '~/components/Footer'
-import Hero from '~/components/Hero'
-import LatestArticles from '~/components/LatestArticles'
+import Prismic from "prismic-javascript";
+import PrismicConfig from "~/prismic.config.js";
+import Navigation from "~/components/Navigation";
+import Footer from "~/components/Footer";
+import BlogHero from "~/components/BlogHero";
+import LatestArticles from "~/components/LatestArticles";
 
 export default {
   components: {
     Navigation,
     Footer,
-    Hero,
+    BlogHero,
     LatestArticles
   },
   head: {
-    title: 'Blog'
+    title: "Blog"
   },
-  async asyncData({context, error, req}) {
+  async asyncData({ context, error, req }) {
     try {
-      const api = await Prismic.getApi(PrismicConfig.apiEndpoint, {req})
-      const navigation = await api.getSingle('navigation')
-      const footer = await api.getSingle('footer')
-      const document = await api.getSingle('blog_home')
-      const posts = await api.query(Prismic.Predicates.at("document.type", "post"))
+      const api = await Prismic.getApi(PrismicConfig.apiEndpoint, { req });
+      const navigation = await api.getSingle("navigation");
+      const footer = await api.getSingle("footer");
+      const document = await api.getSingle("blog_home");
+      const posts = await api.query(
+        Prismic.Predicates.at("document.type", "post")
+      );
 
-      let navigationContent = navigation.data
-      let footerContent = footer.data
-      let blogContent = document.data
-      let postsContent = posts
+      let navigationContent = navigation.data;
+      let footerContent = footer.data;
+      let blogContent = document.data;
+      let postsContent = posts;
 
       return {
         navigationContent,
         footerContent,
         blogContent,
         postsContent
-      }
+      };
     } catch (e) {
-      error(e)
+      error(e);
     }
-  },
-}
+  }
+};
 </script>
 
-<style lang="postcss">
-</style>
+<style lang="postcss"></style>
